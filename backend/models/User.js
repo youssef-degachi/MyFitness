cf1ad3f142842597dbedea91bceed63329745851
youssef-degachi <youssefdegachi0@gmail.com>
2024-12-07 22:58:16 +0100

    i will start again in my backend

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-  full_name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
-
-export const User = mongoose.model('User', userSchema);
