1bfae3a317b3c31b057c59687395c21878b85657
Youssef Degachi <youssefdegachi0@gmail.com>
2025-01-11 22:36:42 +0100

    Delete cleark i don't use it

--- src/utils/cleark.ts
+++
@@ -1,7 +0,0 @@
-// import { Clerk } from '@clerk/clerk-sdk-node';
-
-// export const clerk = new Clerk({
-//   apiKey: import.meta.env.VITE_CLERK_SECRET_KEY,
-// });
-
-// export const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
